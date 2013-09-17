257d275f446eb31c6e595271ef86143377da20d6
Joseph Sheedy <joseph.sheedy@gmail.com>
2013-09-17 16:57:01 -0700

    updates

--- lectures/09-17-2013_Unit-Testing-1/examples/selenium/selenium_test.py
+++ lectures/09-17-2013_Unit-Testing-1/examples/selenium/selenium_test.py
@@ -4,17 +4,21 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Firefox()
-url = "http://localhost:8000/slides/index.html"
-driver.get(url)
-assert "Isilon" in driver.title
 
-body = driver.find_element_by_xpath('//body')
-body.send_keys(Keys.ARROW_RIGHT)
-body.send_keys(Keys.ARROW_RIGHT)
-body.send_keys(Keys.ARROW_RIGHT)
-body.send_keys(Keys.ARROW_RIGHT)
+try:
+    url = "http://localhost:8000/lectures/09-17-2013_Unit-Testing-1/slides/"
+    driver.get(url)
+    assert "Isilon" in driver.title
 
-time.sleep(1)
-elem = driver.find_element_by_id("selenium")
-assert "Selenium" in elem.text
-driver.close()
+    body = driver.find_element_by_xpath('//body')
+    body.send_keys(Keys.ARROW_RIGHT)
+    body.send_keys(Keys.ARROW_RIGHT)
+    body.send_keys(Keys.ARROW_RIGHT)
+    body.send_keys(Keys.ARROW_RIGHT)
+
+    time.sleep(1)
+    elem = driver.find_element_by_id("selenium")
+    assert "Selenium" in elem.text
+
+finally:
+    driver.close()
