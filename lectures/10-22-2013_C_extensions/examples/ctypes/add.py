e2b2a626084d1ce064f0664116c58e1d4661dacf
Joseph Sheedy <joseph.sheedy@gmail.com>
2013-10-21 23:17:41 -0700

    updates

--- lectures/10-22-2013_C_extensions/examples/ctypes/add.py
+++ lectures/10-22-2013_C_extensions/examples/ctypes/add.py
@@ -7,13 +7,11 @@ libc = CDLL("/usr/lib/libc.dylib")
 # libc = CDLL("libc.so")
 libc.printf("printed via libc printf()\n")
 
-# libc.printf("An int %d, a double %f\n", 1234, 3.14)
 libc.printf("An int %d, a double %f\n", 1234, c_double(3.14))
 
 # load the math library
 libm = CDLL("/usr/lib/libm.dylib")
 print libm.pow(3,4)
-
 
 # now load our own shared library
 add = cdll.LoadLibrary("add.so")
